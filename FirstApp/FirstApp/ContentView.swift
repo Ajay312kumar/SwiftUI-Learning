0f4057c9b165f6383c3f72e7efea0b516af8ff90
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-08-03 12:58:19 +0530

    image-added

--- FirstApp/FirstApp/ContentView.swift
+++ FirstApp/FirstApp/ContentView.swift
@@ -14,16 +14,31 @@ struct ContentView: View {
                 .foregroundColor(.red)
             Text("Second")
             Text("Third")
+            Image("doggy")
+                  .resizable()
+                  .frame(width: 100, height: 100)
+                  .foregroundColor(.white)
+                  .clipShape(Circle())
+            AsyncImage(url: URL(string: "https://www.sticklerproofreading.com/wp-content/uploads/2017/02/dog-using-laptop-computer-e1487627703339.jpg")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .clipShape(Rectangle())
+                        .frame(width: 300, height: 300)
+                } else if phase.error != nil {
+                    Text("Failed to load image")
+                } else {
+                    ProgressView("Downloading...")
+                }
+            }
+
+                
         }
-        .padding(5)
-        Spacer(minLength: 5)
         HStack{
             Text("third")
             Text("Four")
         }
         .foregroundColor(.cyan)
-        
-        .padding()
     }
 }
 
